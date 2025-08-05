68ec23a9a62839e7d04a7cba89fa3fd2de4a53ec
ItzSunShine <123359552+ItzSunShine@users.noreply.github.com>
2025-08-05 00:48:21 -0700

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-// === server.js ===
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-// [CONTENT TRUNCATED FOR BREVITY: Will paste the full server.js content in next step]
