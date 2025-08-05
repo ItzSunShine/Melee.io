7cf92cf77db2e2429dc2684bf1c255e11bd6c229
ItzSunShine <123359552+ItzSunShine@users.noreply.github.com>
2025-08-04 23:44:19 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+// === server.js ===
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+// [CONTENT TRUNCATED FOR BREVITY: Will paste the full server.js content in next step]
